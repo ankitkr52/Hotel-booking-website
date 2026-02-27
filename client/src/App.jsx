705f619796a6c5b4250f13dd6d9258a82ca0899e
ankitkr52 <ankit72p@gmail.com>
2026-02-28 01:40:03 +0530

    fixed navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { useLocation } from 'react-router-dom'
 
 const App = () => {
+
+  const isOwnerPath = useLocation().pathname.includes("owner")
+
   return (
     <div>
-      <h1 className='text-3xl'>Welcome to hotel booking app</h1>
+      {!isOwnerPath && < Navbar />}
+     
     </div>
   )
 }
