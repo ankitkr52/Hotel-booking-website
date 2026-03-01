4cc8a954ac7a04ed5ae6252c90921d1d31af3ace
ankitkr52 <ankit72p@gmail.com>
2026-03-02 00:10:11 +0530

    page routing

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import heroImage from '../assets/heroImage.png'  // ✅ Import
+
+const Hero = () => {
+  return (
+    <div 
+      className='flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-no-repeat bg-cover bg-center h-screen'
+      style={{ backgroundImage: `url(${heroImage})` }}  // ✅ Inline style
+    >
+      {/* content */}
+    </div>
+  )
+}
+
+export default Hero
