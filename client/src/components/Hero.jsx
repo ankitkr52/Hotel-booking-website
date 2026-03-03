32e34796348ed908bcb5bc9b6c3672fe8a222690
ankitkr52 <ankit72p@gmail.com>
2026-03-03 11:54:34 +0530

    fixed form section in hero component

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import heroImage from '../assets/heroImage3.png'
+import heroImage from '../assets/heroImage.png'
+import { assets } from '../assets/assets'
+import { cities } from '../assets/assets'
 
 const Hero = () => {
     return (
@@ -7,13 +9,57 @@ const Hero = () => {
             className='flex flex-col items-start justify-center px-4 md:px-16 lg:px-24 xl:px-32 text-white bg-no-repeat bg-cover bg-center h-screen w-full'
             style={{ backgroundImage: `url(${heroImage})` }}
         >
-            <p className='bg-[#49B9FF]/50 px-3.5 py-1 rounded-full mt-20'>The Ultimate Hotel Experiance </p>
+            <p className='bg-[#4334cf]/50 px-4 py-1 rounded-full mt-10'>The Ultimate Hotel Experiance </p>
             <h1 className='text-4xl md:text-6xl font-bold'>Discover Your Perfect Gateway Destination</h1>
-            <p>Unparalleled luxury and comfort await at the world's most exclusive
+            <p className='max-w-130 mt-2 text-sm md:text-base'>Unparalleled luxury and comfort await at the world's most exclusive
                 hotels and resorts. Start your journey today.</p>
             <button className='mt-6 px-8 py-3 bg-white text-black rounded-full hover:bg-gray-100 transition'>
                 Explore Now
             </button>
+
+
+
+            <form className='bg-white text-gray-500 mt-8 rounded-lg px-6 py-4  flex flex-col md:flex-row max-md:items-start gap-4 max-md:mx-auto'>
+
+                <div>
+                    <div className='flex items-center gap-2'>
+                        <img src={assets.calenderIcon} alt="" className='h-4' />
+                        <label htmlFor="destinationInput">Destination</label>
+                    </div>
+                    <input list='destinations' id="destinationInput" type="text" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none" placeholder="Type here" required />
+                    <datalist id='destinations'>
+                    {cities.map((city,index)=>(
+                        <option value={city} key={index}/>
+                    ))}
+                    </datalist>
+                </div>
+
+                <div>
+                    <div className='flex items-center gap-2'>
+                       <img src={assets.calenderIcon} alt="" className='h-4' />
+                        <label htmlFor="checkIn">Check in</label>
+                    </div>
+                    <input id="checkIn" type="date" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none" />
+                </div>
+
+                <div>
+                    <div className='flex items-center gap-2'>
+                       <img src={assets.calenderIcon} alt="" className='h-4' />
+                        <label htmlFor="checkOut">Check out</label>
+                    </div>
+                    <input id="checkOut" type="date" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none" />
+                </div>
+
+                <div className='flex md:flex-col max-md:gap-2 max-md:items-center'>
+                    <label htmlFor="guests">Guests</label>
+                    <input min={1} max={4} id="guests" type="number" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none  max-w-16" placeholder="0" />
+                </div>
+
+                <button className='flex items-center justify-center gap-1 rounded-md bg-black py-3 px-4 text-white my-auto cursor-pointer max-md:w-full max-md:py-1' >
+                   <img src={assets.searchIcon} alt="searchIcon" className='h-7' />
+                    <span>Search</span>
+                </button>
+            </form>
         </div>
     )
 }
