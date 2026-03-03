1a8cfb0eef8acc981ddf5c0d982df8bbdd87b8c7
ankitkr52 <ankit72p@gmail.com>
2026-03-03 18:18:40 +0530

    added sign in or sign up feature using clerk

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { assets } from '../assets/assets';
+import { useClerk, useUser, UserButton } from '@clerk/clerk-react';
 
 const Navbar = () => {
     const navLinks = [
@@ -14,6 +15,9 @@ const Navbar = () => {
 
     const [isScrolled, setIsScrolled] = React.useState(false);
     const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+    const { openSignIn } = useClerk()
+    const { user } = useUser()
 
     React.useEffect(() => {
         const handleScroll = () => {
@@ -47,20 +51,20 @@ const Navbar = () => {
             {/* Desktop Right */}
             <div className="hidden md:flex items-center gap-4">
                 <img src={assets.searchIcon} alt="search" className={`${isScrolled && 'invert'} h-7 transition-all duration-500`} />
-                <button className={` text-white bg-black px-8 py-2.5 rounded-full ml-4 transition-all duration-500`}>
+                <button onClick={openSignIn} className={` text-white bg-black px-8 py-2.5 rounded-full ml-4 transition-all duration-500`}>
                     Login
                 </button>
             </div>
 
             {/* Mobile Menu Button */}
             <div className="flex items-center gap-3 md:hidden">
-                <img onClick={()=>setIsMenuOpen(!isMenuOpen)} src={assets.menuIcon} alt="" className={`${isScrolled && 'invert'} h-4`} />
+                <img onClick={() => setIsMenuOpen(!isMenuOpen)} src={assets.menuIcon} alt="" className={`${isScrolled && 'invert'} h-4`} />
             </div>
 
             {/* Mobile Menu */}
             <div className={`fixed top-0 left-0 w-full h-screen bg-white text-base flex flex-col md:hidden items-center justify-center gap-6 font-medium text-gray-800 transition-all duration-500 ${isMenuOpen ? "translate-x-0" : "-translate-x-full"}`}>
                 <button className="absolute top-4 right-4" onClick={() => setIsMenuOpen(false)}>
-                  <img src={assets.closeIcon} alt="close-menu" className='h-6.5'/>
+                    <img src={assets.closeIcon} alt="close-menu" className='h-6.5' />
                 </button>
 
                 {navLinks.map((link, i) => (
@@ -73,7 +77,7 @@ const Navbar = () => {
                     Dashboard
                 </button>
 
-                <button className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500">
+                <button onClick={openSignIn} className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500">
                     Login
                 </button>
             </div>
