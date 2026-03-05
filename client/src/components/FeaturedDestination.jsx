a32719a7106ac56ef2427f5d2aea2300485731ed
ankitkr52 <ankit72p@gmail.com>
2026-03-05 23:06:36 +0530

    fixed feature card section with title

--- client/src/components/FeaturedDestination.jsx
+++ client/src/components/FeaturedDestination.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { roomsDummyData } from '../assets/assets'
 import HotelCard from './HotelCard'
+import Title from './Title'
 
 const FeaturedDestination = () => {
   return (
     <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20'>
+     <Title title="Featured Destinations" subTitle="Discover our most popular destinations" />
       <div className='flex flex-wrap items-center justify-center gap-6 mt-20'>
         {roomsDummyData.slice(0, 4).map((room, index) => (
             <HotelCard room={room} index={index} key={room._id} />
---
+++ client/src/components/Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Title = ({ title, subTitle, align, font }) => {
+  return (
+    <div className={`flex items-center flex-col justify-center text-center ${align === 'left' && "md:items-start md:text-left"}`}>
+      <h1 className={`text-4xl md:text-[40px] ${font || "font-playfair"}`}>{title}</h1>
+        < p className='text-sm md:text-base text-gray-600 mt-2 max-w-174 ' > { subTitle }</p>
+    </div >
+  )
+}
+
+export default Title
