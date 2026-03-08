f4bbf6427352c8ec0a538f94b5b8d39a6d025ba8
ankitkr52 <ankit72p@gmail.com>
2026-03-08 20:06:41 +0530

    update testimonial section

---
+++ client/src/components/Testimonial.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Title from './Title'
+
+const Testimonial = () => {
+  return (
+    <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 pt-20 pb-30 bg-slate-50'>
+      <Title title="What Our Guests Say" subTitle="Discover why discerning travelers choose QuickStay for their luxury accommodations around the world."/>
+    </div>
+  )
+}
+
+export default Testimonial
