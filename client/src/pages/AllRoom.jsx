a426ce71639bc939cea220df0de261a86580e290
ankitkr52 <ankit72p@gmail.com>
2026-03-24 19:56:05 +0530

    update allroom section

--- client/src/pages/AllRoom.jsx
+++ client/src/pages/AllRoom.jsx
@@ -4,9 +4,9 @@ const AllRoom = () => {
     return (
         <div className='flex flex-col-reverse lg:flex-row items-start justify-between pt-28 md:pt-35 px-4 md:px-16 lg:px-24 xl:px-32'>
             <div>
-                <div>
+                <div className='flex flex-col items-start text-left'>
                     <h1 className='font-playfair text-4xl md:text-[40px]'>Hotels Rooms</h1>
-                    <p className=''>Take advantage of our limited-time offers and special packages to enhance your stay and create unforgettable memories.</p>
+                    <p className='text-sm md:text-base text-gray-500/90 mt-2 max-w-174'>Take advantage of our limited-time offers and special packages to enhance your stay and create unforgettable memories.</p>
                 </div>
             </div>
             {/* filters */}
