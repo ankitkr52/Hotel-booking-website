f59a607ff80dc4369973eac801434be825654b33
ankitkr52 <ankit72p@gmail.com>
2026-04-03 23:42:25 +0530

    update owner dashboard section

--- client/src/components/hotelowner/Sidebar.jsx
+++ client/src/components/hotelowner/Sidebar.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
+import { NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
+
+  const sidebarLinks=[
+    {name:"Dashboard",path:"/owner",icons:assets.dashboardIcon},
+    {name:"Add Room",path:"/owner/add-room",icons:assets.addIcon},
+    {name:"List Room",path:"/owner/list-room",icons:assets.listIcon},
+  ]
+
   return (
-    <div>
-      
+    <div className='md:w-64 w-16 border-r h-full text-base border-gray-500 pt-4 flex flex-col transition-all duration-300'>
+      {sidebarLinks.map((items,idex)=>(
+        <NavLink to={items.path} key={index} end="/owner" className={}>
+
+        </NavLink>
+      ))}
     </div>
   )
 }
