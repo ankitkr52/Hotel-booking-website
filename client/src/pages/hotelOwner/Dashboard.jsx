693c5a5db6d224130383d9f3e87c4090fb3b89a3
ankitkr52 <ankit72p@gmail.com>
2026-04-19 23:05:52 +0530

    update dashboard section

--- client/src/pages/hotelOwner/Dashboard.jsx
+++ client/src/pages/hotelOwner/Dashboard.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Title from '../../components/Title'
+import { assets, dashboardDummyData } from '../../assets/assets'
 
 const Dashboard = () => {
+  const [dashboardData, setDashboardData] = useState(dashboardDummyData)
   return (
     <div>
-      <Title  align="left" font="outfit" title="Dashboard" subTitle="Moniter your room listing, track bookings and analyis revenue-all in one place. stay updated with real-time insights to ensure smooth operation"/>
+      <Title align="left" font="outfit" title="Dashboard" subTitle="Moniter your room listing, track bookings and analyis revenue-all in one place. stay updated with real-time insights to ensure smooth operation." />
+
+      <div className='flex gap-8 my-8'>
+        {/* Total booking */}
+        <div className='bg-primary/3 border border-primary/10 rounded flex p-4 pr-8'>
+          <img src={assets.totalBookingIcon} alt="" className='max-sm:hidden h-10' />
+          <div className='flex flex-col sm:ml-4 font-medium'>
+            <p className='text-blue-500 text-lg'>Total Bookings</p>
+            <p className='text-neutral-400 text-base'>{dashboardData.totalBookings}</p>
+          </div>
+
+        </div>
+        {/* Total Revenue */}
+        <div className='bg-primary/3 border border-primary/10 rounded flex p-4 pr-8'>
+          <img src={assets.totalRevenueIcon} alt="" className='max-sm:hidden h-10' />
+          <div className='flex flex-col sm:ml-4 font-medium'>
+            <p className='text-blue-500 text-lg'>Total Revenue</p>
+            <p className='text-neutral-400 text-base'>${dashboardData.totalRevenue}</p>
+          </div>
+
+        </div>
+      </div>
+      {/* Recent booking */}
+      <h2 className='text-xl text-blue-950/70 font-medium mb-5'>Recent Bookings</h2>
     </div>
   )
 }
