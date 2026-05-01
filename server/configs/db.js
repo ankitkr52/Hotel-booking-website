c46bc766cb925d0d8429729bd6e281071f57c5b6
ankitkr52 <ankit72p@gmail.com>
2026-05-01 11:47:48 +0530

    Initial backend setup

---
+++ server/configs/db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        mongoose.connection.on("connected", () => console.log("database connected")
+    );
+        
+        await mongoose.connect(process.env.MONGODB_URI, {
+            serverSelectionTimeoutMS: 8000,
+        });
+ 
+
+        
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from "express"
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+
+connectDB();
+
+const app = express();
+app.use(cors())  // enable cross origin resorce sharing
+
+app.get('/',(req,res)=>res.send("API is working here"))
+
+const PORT =process.env.PORT || 3000;
+app.listen(PORT,()=>console.log(`server is running on port ${PORT}`))
