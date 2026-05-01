2a58f30c98c4ad47832a83527857962431e2f5a7
ankitkr52 <ankit72p@gmail.com>
2026-05-01 12:21:51 +0530

    update config file to connect database

--- server/configs/db.js
+++ server/configs/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     );
         
         await mongoose.connect(process.env.MONGODB_URI, {
-            serverSelectionTimeoutMS: 8000,
+           
         });
  
 
