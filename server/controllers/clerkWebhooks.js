9b5d6ab957f72cacb52ce20bcfbe5121642849a9
ankitkr52 <ankit72p@gmail.com>
2026-05-04 22:36:55 +0530

    update cleark api endpoint

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -1,27 +1,56 @@
-import User from "../models/User";
-import { Webhook } from "svix"
-
+const User = require("../models/User");
+const { Webhook } = require("svix");
 
 const clerkWebhooks = async (req, res) => {
     try {
-        // create a webhook instance with the secret key
-        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
-        // getting headers
+        // Webhook instance with secret key
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+
+        // Getting headers
         const headers = {
             'svix-id': req.headers['svix-id'],
             'svix-timestamp': req.headers['svix-timestamp'],
             'svix-signature': req.headers['svix-signature'],
+        };
+
+        // Verifying headers
+        await whook.verify(JSON.stringify(req.body), headers);
+
+        // Getting data from request body
+        const { data, type } = req.body;
+
+        const userData = {
+            _id: data.id,
+            email: data.email_addresses[0].email_address,
+            username: data.first_name + " " + data.last_name,
+            image: data.image_url,  // ✅ lowercase 'i'
+        };
+
+        // Switch case for different event types
+        switch (type) {
+            case "user.created":
+                await User.create(userData);
+                break;
+
+            case "user.updated":
+                await User.findByIdAndUpdate(data.id, userData);
+                break;
+
+            case "user.deleted":
+                await User.findByIdAndDelete(data.id);
+                break;
+
+            default:
+                break;
         }
-        // verifying headers
-        await whook.verify(JSON.stringify(req.body), headers)
-        // getting ddata from the request body
-        const { data,type }=req.body
+
         
-        const userdata={
-            
-        }
-    }
-    catch (error) {
+        res.json({ message: "Webhook received successfully" });
+
+    } catch (error) {
         console.error("Error processing webhook:", error);
+        res.status(500).json({ message: "Webhook error", error: error.message });
     }
-}
+};
+
+export default clerkWebhooks;
