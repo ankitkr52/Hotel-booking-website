6ac55652d9237e5272dc05b9b98ce02a82722b22
ankitkr52 <ankit72p@gmail.com>
2026-05-06 21:49:34 +0530

    fixed depolyment issues

--- server/server.js
+++ server/server.js
@@ -2,8 +2,6 @@ import express from "express"
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
-import { clerkMiddleware } from '@clerk/express'
-import clerkWebhooks from "./controllers/clerkWebhooks.js";
 
 connectDB();
 
@@ -11,13 +9,7 @@ const app = express();
 
 app.use(cors())
 app.use(express.json())
-app.use(clerkMiddleware())
 
-// webhook route
-app.use("/api/clerk", clerkWebhooks)
-
-// test route
 app.get('/', (req, res) => res.send("API is working here"))
-
 
 export default app;
