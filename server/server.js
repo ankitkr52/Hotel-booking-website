e698da146b2ba226fdcde2d2f3dba8e01f5f22a3
ankitkr52 <ankit72p@gmail.com>
2026-05-07 00:03:01 +0530

    fixed deployment issues in clerkmiddleware

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express"
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
+import { clerkMiddleware } from '@clerk/express'
+import clerkWebhooks from "./controllers/clerkWebhooks.js";
 
 connectDB();
 
@@ -9,7 +11,13 @@ const app = express();
 
 app.use(cors())
 app.use(express.json())
+app.use(clerkMiddleware())
 
+// webhook route
+app.use("/api/clerk", clerkWebhooks)
+
+// test route
 app.get('/', (req, res) => res.send("API is working here"))
+
 
 export default app;
