4fb0d9bf7cb17d8aa4d4c4915ccbece04b26b113
ankitkr52 <ankit72p@gmail.com>
2026-05-14 11:30:39 +0530

    Update backend URL for Vercel deployment

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
+import clerkWebhooks from "./controllers/clerkWebhooks.js";
 
 connectDB();
 
@@ -10,6 +11,12 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
+app.post("/webhooks/clerk", clerkWebhooks)
+
 app.get('/', (req, res) => res.send("API is working here"))
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server is running")
+})
 
 export default app;
