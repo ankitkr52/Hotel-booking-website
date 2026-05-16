eaed0ed60837b6277fc07afa54de2050ff3f6aad
ankitkr52 <ankit72p@gmail.com>
2026-05-16 16:25:42 +0530

    added routes for roomrouter

--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -1,13 +1,16 @@
 import express from "express"
 import upload from "../middleware/uploadMiddleware.js"
 import { protect } from "../middleware/authMiddleware.js"
-import { createRoom } from "../controllers/roomController.js"
+import { createRoom, getOwnerRooms, getRoom, toggelRoomAvailability } from "../controllers/roomController.js"
 
 
 
 const roomRouter=express.Router()
 
 roomRouter.post("/",protect,upload.array("images",4),createRoom)
+roomRouter.get("/",getRoom)
+roomRouter.get("/owner",protect,getOwnerRooms)
+roomRouter.post("/toggle-availability",protect,toggelRoomAvailability)
 
 
 
