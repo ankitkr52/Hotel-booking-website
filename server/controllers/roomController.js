c65d21f952d803f7f3d51e3c79da7aed1a74d7f6
ankitkr52 <ankit72p@gmail.com>
2026-05-16 16:19:03 +0530

    create api for get room amd toggle room availabity

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -35,17 +35,43 @@ export const createRoom = async (req, res) => {
 // API to get all room 
 
 export const getRoom = async (req, res) => {
+    try {
+        const rooms = await Room.find({ isAvailable: true }).populate({
+            path: 'hotel',
+            populate: {
+                path: 'owner',
+                select: 'image'
+            }
+        }).sort({ createdAt: -1 })
+        res.json({ success: true, rooms })
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
 
 }
 
 // API to get all room for specific hotel
 
 export const getOwnerRooms = async (req, res) => {
-
+    try {
+        const hotelData = await Hotel({ owner: req.auth.userId })
+        const rooms = await Room.find({ hotel: hotelData._id.toString() }).populate("hotel")
+        res.json({ success: true, rooms })
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
 }
 
 // API to toggel availabity of a room 
 
 export const toggelRoomAvailability = async (req, res) => {
-
+try {
+    const {roomId}=req.body
+    const roomData=await Room.findById(roomId)
+    roomData.isAvailable=!roomData.isAvailable;
+    await roomData.save()
+    res.json({success:true,message:"Room availability update "})
+} catch (error) {
+    res.json({ success: false, message: error.message })
+}
 }
