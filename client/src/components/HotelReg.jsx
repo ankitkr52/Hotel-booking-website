8888c8481190ce209aa709766ae927d001b1aaea
ankitkr52 <ankit72p@gmail.com>
2026-06-10 17:39:15 +0530

    update state variable in hotelreg file

--- client/src/components/HotelReg.jsx
+++ client/src/components/HotelReg.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets, cities } from '../assets/assets'
 import { useAppContext } from '../context/appContext'
 
 const HotelReg = () => {
     const {setShowHotelReg}=useAppContext()
+
+    const [name,setName]=useState("")
+    const [address,setAddress]=useState("")
+    const [contact,setContact]=useState("")
+    const [city,setCity]=useState("")
     return (
         <div className='fixed top-0 bottom-0 left-0 right-0 z-100 flex items-center justify-center bg-black/70'>
             <form className='flex bg-white rounded-xl max-w-4xl max-md:mx-2'>
@@ -13,26 +18,26 @@ const HotelReg = () => {
                     <p className='text-2xl font-semibold mt-6'>Register Your Hotel</p>
                     <div className='w-full mt-4'>
                         <label htmlFor="name" className='font-medium text-gray-500'>Hotel Name <span className='text-red-500'>*</span></label>
-                        <input id='name' type="text" placeholder='Type-here' className='border border-gray-200 rounded w-full px-3 py-2.5 mt-2 outline-orange-500 font-light ' required />
+                        <input id='name'onChange={(e)=>setName(e.target.value)} value={name} type="text" placeholder='Type-here' className='border border-gray-200 rounded w-full px-3 py-2.5 mt-2 outline-orange-500 font-light ' required />
 
                     </div>
                     {/* Phone */}
                     <div className='w-full mt-4'>
                         <label htmlFor="contact" className='font-medium text-gray-500'>Phone <span className='text-red-500'>*</span></label>
-                        <input id='contact' type="text" placeholder='Type-here' className='border border-gray-200 rounded w-full px-3 py-2.5 mt-2 outline-orange-500 font-light ' required />
+                        <input id='contact' type="text" onChange={(e)=>setContact(e.target.value)} value={contact} placeholder='Type-here' className='border border-gray-200 rounded w-full px-3 py-2.5 mt-2 outline-orange-500 font-light ' required />
 
                     </div>
                     {/* adderess */}
                     <div className='w-full mt-4'>
                         <label htmlFor="Address" className='font-medium text-gray-500'>Address <span className='text-red-500'>*</span></label>
-                        <input id='Address' type="text" placeholder='Type-here' className='border border-gray-200 rounded w-full px-3 py-2.5 mt-2 outline-orange-500 font-light' required />
+                        <input id='Address'onChange={(e)=>setAddress(e.target.value)} value={address} type="text" placeholder='Type-here' className='border border-gray-200 rounded w-full px-3 py-2.5 mt-2 outline-orange-500 font-light' required />
 
                     </div>
                     {/* Select city Dropdown */}
                     <div className='w-full mt-4 max-w-60 mr-auto'>
                         <label htmlFor="city" className='font-medium text-gray-500'>City <span className='text-red-500'>*</span></label>
                         
-                        <select id="city" className='border border-gray-200 cursor-pointer rounded w-full px-3 py-2.5 mt-2 outline-orange-500 font-light'>
+                        <select id="city" onChange={(e)=>setCity(e.target.value)} value={city} className='border border-gray-200 cursor-pointer rounded w-full px-3 py-2.5 mt-2 outline-orange-500 font-light'>
                             <option value="">Select City</option>
                             {cities.map((city) => (
                                 <option key={city} value={city}>{city}</option>
