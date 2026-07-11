5ee8c7a98c8ab9094f91f0887c991a100cfc6540
ankitkr52 <ankit72p@gmail.com>
2026-07-11 13:44:38 +0530

    project comleted ready for deployment

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -19,26 +19,45 @@ const clerkWebhooks = async (req, res) => {
         // Getting data from request body
         const { data, type } = req.body;
 
-        const userData = {
-            _id: data.id,
-            email: data.email_addresses[0].email_address,
-            username: data.first_name + " " + data.last_name,
-            image: data.image_url,
-        };
+
 
         // Switch case
         switch (type) {
-            case "user.created":
+            case "user.created": {
+                const userData = {
+                    _id: data.id,
+                    email: data.email_addresses[0].email_address,
+                    username: data.first_name + " " + data.last_name,
+                    image: data.image_url,
+                };
+
                 await User.create(userData);
                 break;
+            }
 
-            case "user.updated":
+            case "user.updated": {
+                const userData = {
+                    _id: data.id,
+                    email: data.email_addresses[0].email_address,
+                    username: data.first_name + " " + data.last_name,
+                    image: data.image_url,
+                };
+
                 await User.findByIdAndUpdate(data.id, userData);
                 break;
+            }
 
-            case "user.deleted":
+            case "user.deleted": {
+                const userData = {
+                    _id: data.id,
+                    email: data.email_addresses[0].email_address,
+                    username: data.first_name + " " + data.last_name,
+                    image: data.image_url,
+                };
+
                 await User.findByIdAndDelete(data.id);
                 break;
+            }
 
             default:
                 break;
